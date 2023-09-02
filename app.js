1c705ba919b80baeddfe6005f53a4c97acb645fb
yoshimura178 <143882500+yoshimura178@users.noreply.github.com>
2023-09-02 23:21:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ button.addEventListener("click", function(){
 function progressInterva(){
     if(count == 100 && percentWidth == 100){
         clearInterval(progressLoad)
-        percent.textContent = "Ok rùi đó <3)";
+        percent.textContent = "Ok rùi đó <3";
         percent.style.letterSpacing = "1px";
         textBox.style.transform = "scale(1.3)"
         heartItem3.style.animation = "1s heartScale forwards"
